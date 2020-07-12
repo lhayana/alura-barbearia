270f871f007d257be728872a1402f431f2ce0d38
Travis CI <travis@travis-ci.org>
2020-07-11 22:58:45 -0300

    up

---
+++ projeto-POO-banco/Conta.js
@@ -0,0 +1,50 @@
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
+       
+       if (this.constructor == Conta){
+            throw new Error("Você não deve instanciar um objeto tipo Conta diretamente. Classe abstrata.");
+        }
+
+        this._saldo = saldoInicial;
+        this._cliente = cliente;
+        this._agencia = agencia;
+    }
+
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    //método abstrato: feito para ser sobrescrito 
+    sacar(valor) {
+        throw new Error("O método Sacar é abstrato")
+    }
+
+    _sacar(valor, taxa) {
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
+        }
+
+        return 0;
+    }
+
+    depositar(valor) {
+        this._saldo += valor;
+    }
+
+    tranferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
